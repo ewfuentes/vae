bc718f912827aa19bc77c0ee967e580c0270335f
Erick Fuentes <efuentes@mit.edu>
2026-07-05 18:24:07 -0400

    add validation metrics

--- train_classifier.py
+++ train_classifier.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 import torch.nn.functional as F
+import torchmetrics.functional.classification
 
 from classifier import Classifier, ClassifierConfig
 from dataset import load_dataset
@@ -13,14 +14,17 @@ def main(output_dir: Path):
         train_dataset, shuffle=True, batch_size=1024
     )
 
-    test_dataset = load_dataset(train=False)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1024)
+    validation_dataset = load_dataset(train=False)
+    validation_dataloader = torch.utils.data.DataLoader(
+        validation_dataset, batch_size=1024
+    )
 
-    num_epochs = 100
+    num_epochs = 25
 
     model = Classifier(
         ClassifierConfig(channel_dims=[8, 16, 32, 64], out_dim=10)
     ).cuda()
+    model.train()
 
     opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 
@@ -36,6 +40,27 @@ def main(output_dir: Path):
                 f"{epoch_idx=} {batch_idx=} loss={loss.detach().item():0.3f}", end="\r"
             )
         print()
+
+        # compute validation loss
+        output_preds = []
+        output_labels = []
+        model.eval()
+        with torch.no_grad():
+            for img, label in validation_dataloader:
+                output_preds.append(F.softmax(model(img.cuda()), dim=-1).cpu())
+                output_labels.append(label)
+
+        output_preds = torch.cat(output_preds, dim=0)
+        output_labels = torch.cat(output_labels, dim=0)
+        auroc = torchmetrics.functional.classification.multiclass_auroc(
+            output_preds, output_labels, num_classes=10
+        )
+        accuracy = torchmetrics.functional.classification.multiclass_accuracy(
+            output_preds, output_labels, num_classes=10
+        )
+        print(f"{auroc=:0.3f} {accuracy=:0.3f}")
+
+        model.train()
 
 
 if __name__ == "__main__":
